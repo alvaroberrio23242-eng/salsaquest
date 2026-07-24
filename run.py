d8ce77f2b45366df958c18adb529a5479c3aa173
benjapc <berriobenjamin16@gmail.com>
2026-07-24 03:53:21 -0500

    Fix: Limpieza de run.py para Gunicorn y Render

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
-# run.py
-
 """
-Punto de entrada de la aplicación.
-Este es el archivo que se ejecuta para levantar el servidor de desarrollo.
+Punto de entrada de la aplicación SalsaQuest.
+Inicializa la base de datos y levanta el servidor Flask / WSGI.
 """
 
-from app import crear_app
+from app import crear_app, db
 
 app = crear_app()
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
